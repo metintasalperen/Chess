49d979301d4ddd37d6e247d12bf79b09bb98d8cc
metintasalperen <55763726+metintasalperen@users.noreply.github.com>
2022-12-30 23:11:02 +0300

    Add constructor to ChessLogicCls

--- Chess/ChessLogicCls.cpp
+++ Chess/ChessLogicCls.cpp
@@ -1,1 +1,81 @@
 #include "ChessLogicCls.h"
+#define DEBUG
+
+#ifdef DEBUG
+#include <QDebug>
+#endif // DEBUG
+
+ChessLogicCls::ChessLogicCls()
+{
+	memset(&TileArr[0], 0, sizeof(TileStc) * Tile_Count);
+
+	for (int i = 0; i < Tile_Count; i++)
+	{
+		if (i <= Tile_H2)
+		{
+			TileArr[i].tileStatus = TileStatus_NotEmpty;
+			TileArr[i].piece.player = Player_White;
+			TileArr[i].piece.pieceId = static_cast<PieceEnum>(i);
+
+			if (i >= Tile_A2)
+			{
+				TileArr[i].piece.pieceType = PieceType_Pawn;
+			}
+            else if ((i == Tile_A1) || (i == Tile_H1))
+            {
+                TileArr[i].piece.pieceType = PieceType_Rook;
+            }
+            else if ((i == Tile_B1) || (i == Tile_G1))
+            {
+                TileArr[i].piece.pieceType = PieceType_Knight;
+            }
+            else if ((i == Tile_C1) || (i == Tile_F1))
+            {
+                TileArr[i].piece.pieceType = PieceType_Bishop;
+            }
+            else if (i == Tile_D1)
+            {
+                TileArr[i].piece.pieceType = PieceType_Queen;
+            }
+            else if (i == Tile_E1)
+            {
+                TileArr[i].piece.pieceType = PieceType_King;
+            }
+		}
+        else if (i >= Tile_A7)
+        {
+            TileArr[i].tileStatus = TileStatus_NotEmpty;
+            TileArr[i].piece.player = Player_Black;
+            TileArr[i].piece.pieceId = static_cast<PieceEnum>(i - 32);
+
+            if (i <= Tile_H7)
+            {
+                TileArr[i].piece.pieceType = PieceType_Pawn;
+            }
+            else if ((i == Tile_A8) || (i == Tile_H8))
+            {
+                TileArr[i].piece.pieceType = PieceType_Rook;
+            }
+            else if ((i == Tile_B8) || (i == Tile_G8))
+            {
+                TileArr[i].piece.pieceType = PieceType_Knight;
+            }
+            else if ((i == Tile_C8) || (i == Tile_F8))
+            {
+                TileArr[i].piece.pieceType = PieceType_Bishop;
+            }
+            else if (i == Tile_D8)
+            {
+                TileArr[i].piece.pieceType = PieceType_Queen;
+            }
+            else if (i == Tile_E8)
+            {
+                TileArr[i].piece.pieceType = PieceType_King;
+            }
+        }
+        else
+        {
+            TileArr[i].tileStatus = TileStatus_Empty;
+        }
+	}
+}
--- Chess/ChessLogicCls.h
+++ Chess/ChessLogicCls.h
@@ -1,8 +1,17 @@
 #ifndef CHESSLOGICCLS_H
 #define CHESSLOGICCLS_H
 
+#include "StructPkg.h"
+
 class ChessLogicCls
 {
+public:
+    // Attributes
+    TileStc TileArr[Tile_Count];
+
+    // Methods
+    // Default constructor
+    ChessLogicCls();
 };
 
 #endif // !CHESSLOGICCLS_H
